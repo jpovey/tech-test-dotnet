9b937cb6f090ce7adc1e2b9321f25b5c525d15ae
Jon Povey <jonathan.povey@asos.com>
2025-12-01 21:38:24 +0000

    Add test base

---
+++ ClearBank.DeveloperTest.Tests/Services/PaymentServiceShould.cs
@@ -0,0 +1,14 @@
+﻿namespace ClearBank.DeveloperTest.Tests.Services
+{
+    using DeveloperTest.Services;
+
+    public class PaymentServiceShould
+    {
+        private readonly PaymentService _sut;
+
+        public PaymentServiceShould()
+        {
+            _sut = new PaymentService();
+        }
+    }
+}
